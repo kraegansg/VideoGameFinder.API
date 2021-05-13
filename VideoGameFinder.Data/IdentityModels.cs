f664b120585ca083875793758d4c44fd612e01da
ksgraves <kraegan.s.g@gmail.com>
2021-05-13 18:31:50 -0400

    Initial push of Kraegan's GameGenre Branch

--- VideoGameFinder.Data/IdentityModels.cs
+++ VideoGameFinder.Data/IdentityModels.cs
@@ -12,7 +12,7 @@ namespace VideoGameFinder.API.Models
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType);
+            var userIdentity = await manager.CreateIdentityAsync(this, authenticationType); 
             // Add custom user claims here
             return userIdentity;
         }
