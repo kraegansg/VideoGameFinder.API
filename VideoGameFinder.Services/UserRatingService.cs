f2a59712b83a2ff8c61900f282067de031d80b9f
Charles Lucas <charlesdlucas1975@gmail.com>
2021-05-13 23:06:41 -0400

    Charles UserRating update

---
+++ VideoGameFinder.Services/UserRatingService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace VideoGameFinder.Services
+{
+    public class UserRatingService
+    {
+        public bool CreateUserRating(UserRatingCreate model)
+        {
+            var entity =
+                new UserRating()
+                {
+                    GameTitle = model.GameTitle
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.UserRatings.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<UserRatingListItem> GetUserRatings()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .UserRatings
+                    .Select(
+                        e =>
+                        new UserRatingListItem
+                        {
+                            UserId = e.UserId,
+                            GameTitle = e.GameTitle
+                        });
+                return query.ToArray();
+            }
+        }
+
+        public UserRatingDetail GetUserRatingbyId (int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+
+            }
+        }
+    }
+
+    internal class UserRatingListItem
+    {
+        public object UserId { get; set; }
+        public object GameTitle { get; set; }
+    }
+}
