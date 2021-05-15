bebf4498591ab1d16631c9e287688ea28e3435ef
Charles Lucas <charlesdlucas1975@gmail.com>
2021-05-15 12:26:48 -0400

    further updated corrected errors

--- VideoGameFinder.API/Providers/ApplicationOAuthProvider.cs
+++ VideoGameFinder.API/Providers/ApplicationOAuthProvider.cs
@@ -10,6 +10,7 @@ using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.OAuth;
 using VideoGameFinder.API.Models;
+using VideoGameFinder.Data;
 
 namespace VideoGameFinder.API.Providers
 {
