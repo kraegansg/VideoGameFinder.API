2c16695d10905078bd6e942503fcd825c465b2f1
ksgraves <kraegan.s.g@gmail.com>
2021-05-18 17:43:56 -0400

    Updated references and using statemetns in service model and data layers

--- VideoGameFinder.Services/GameGenreService.cs
+++ VideoGameFinder.Services/GameGenreService.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VideoGameFinder.Data;
+using VideoGameFinder.Models;
 
 namespace VideoGameFinder.Services
 {
     public class GameGenreService
     {
-        private readonly
+        private readonly Guid _userId;
+
+        public GameGenreService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+
+        public bool CreateGameGenre(GameGenreCreate model)
+        {
+            var entity =
+                new GameGenre()
+                {
+                    OwnerId = _userId,
+                    GenreType = model.GenreType
+
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.GameGenres.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
