4ceb786a540d14e4814e90d1899e9d54a5815da3
Greg Gonzalez <gregery.gonzalez@gmail.com>
2021-05-18 12:30:12 -0400

    Created Game Models, updated Game Class in Data layer

--- VideoGameFinder.Data/Game.cs
+++ VideoGameFinder.Data/Game.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,20 @@ namespace VideoGameFinder.Data
 {
     public class Game
     {
+        [Key]
+        public int GameId { get; set; }
+        public string GameTitle { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int PlayerCount { get; set; }
 
+        [ForeignKey]
+        public int GameSystemId { get; set; }
+        [ForeignKey]
+        public int GameGenreId { get; set; }
+        public string ESRBRating { get; set; }
+        public bool IsReccommended { get; set; }
+        public double GamePrice { get; set; }
+        public string GameGenre { get; set; }
+        public string GameSystem { get; set; }
     }
 }
